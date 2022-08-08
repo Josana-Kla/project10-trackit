9f2f5edbe3cf6638ed146628120d5fc009d9a33a
Josana-Kla <josana.0205@gmail.com>
2022-08-07 23:02:22 -0300

    style: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import UserDatas from "../contexts/UserDatas";
+import HeaderStyle from "../components/HeaderStyle";
 
 export default function Header() {
     const { userDatas, setUserDatas } = useContext(UserDatas);
-    console.log(userDatas)
     return(
-        <header>
+        <HeaderStyle>
             <img src="../../../assets/img/small-logo.svg" alt="small-logo" />
             <img src={userDatas.image} alt="profile" />
-        </header>
+        </HeaderStyle>
     )
 }
--- src/components/create-habits/CreateHabits.js
+++ src/components/create-habits/CreateHabits.js
@@ -60,7 +60,7 @@ function DayButton( { day , idNumber, setWeekdayNumbers, weekdayNumbers, loading
     const [ color, setColor ] = useState("");
    
     //IMPORTANT: Nessa função, eu pego o elemento que cliquei, adiciono a cor, e depois crio um novo array contendo o id do elemento clicado, mas só se o id já não estiver dentro do array. Depois faço um filter para tirar os valores que ficam undefined quando eu desclico.
-    // TODO: Acrescentar um sort no array weekdayNumbers 
+    
     function daySelected(idChosen) {
         if(color === "") {
             setColor("day-selected");
